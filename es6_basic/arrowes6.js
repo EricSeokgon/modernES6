1fcfd28bc0abefb2f44b42411cb134b1e394f581
hadesyi <leesk55@gmail.com>
2019-12-23 17:26:32 +0900

    화살표 함수 문법

--- es6_basic/arrowes6.js
+++ es6_basic/arrowes6.js
@@ -9,3 +9,13 @@ const b = () => {
     console.log("arrow function");
 };
 b();
+
+const print = text => {
+    console.log(text);
+};
+//파라미터가 1개일땐 관호 생략 가능
+print("test");
+
+const sum = (a, b) => (a + b);
+//간단한 표현식만을 반환 할때는 return 생략 가능
+console.log(sum(1,3));
