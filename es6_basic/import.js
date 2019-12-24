9a407f40e001524bcb25aeaa6bf460687cd04e82
hadesyi <leesk55@gmail.com>
2019-12-24 09:24:20 +0900

    모듈 export, import

---
+++ es6_basic/import.js
@@ -0,0 +1,3 @@
+/*import {name1, name2} from './module';*/
+import * as module from './module';
+module.sum1(1, 3);
---
+++ es6_basic/module.js
@@ -0,0 +1,12 @@
+export const someConst = 5;
+
+export function sum(a, b) {
+    return a + b;
+}
+
+
+const someConst1 = 5;
+const sum1 = function (a, b) {
+    return a + b;
+}
+export {someConst1, sum1}
